50c07a31dd96523c1f00cc83c708af552ce7f502
cps97 <colinstoutt@gmail.com>
2022-12-14 21:40:51 -0800

    init commit

---
+++ models/survey.js
@@ -0,0 +1,8 @@
+// const mongoose = require("mongoose");
+
+// const productSchema = new mongoose.Schema({
+
+// });
+
+// const products = mongoose.model("surveys", productSchema);
+// module.exports = products;
